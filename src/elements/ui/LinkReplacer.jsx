5082f4529650eed9c2942b79daae7e05f09945a8
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-01-29 22:00:54 +0200

    yes

--- src/elements/ui/LinkReplacer.jsx
+++ src/elements/ui/LinkReplacer.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 
-const LinkReplacer = ({ text }) => {
+const LinkReplacer  = ({ text }) => {
     // Regular expression to find links in the text
     const linkRegex = /(https:\/\/\S+)/g;
   
     // Replace links in the text with <a> tags
-    const jsxElement = text.replace(linkRegex, (link) => (
-      <a key={link} href={link} target="_blank" rel="link">
-        click me!
-      </a>
-    ));
+    const jsxElement = (
+      <p>
+        {text.replace(linkRegex, (match, link) => (
+          <span key={link}>
+            {text.indexOf(match) > 0 ? ' ' : ''}
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              Click me
+            </a>
+          </span>
+        ))}
+      </p>
+    );
   
     return <div>{jsxElement}</div>;
   };
